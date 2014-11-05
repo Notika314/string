10b1ae4a9cf1254fad3eccff99948213faed833c
Natalia Bryzhatenko <notika314@gmail.com>
2014-11-05 12:27:09 -0500

    counts all letters in a string

---
+++ letter_count.rb
@@ -0,0 +1,13 @@
+def letter_count(string)
+  letter_count={}
+  all_letters=string.gsub(" ", "").split("")
+  all_letters.each do |letter|
+    if letter_count.keys.include?(letter)
+      letter_count[letter]=letter_count[letter]+1
+    else
+      letter_count[letter]=1
+    end
+  end
+    letter_count
+end
+ puts letter_count("moon")
